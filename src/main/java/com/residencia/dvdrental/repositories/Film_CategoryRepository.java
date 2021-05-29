484282af6375f8427bac86aceef6ca1201fd52b7
mpj144 <marcelopjrdev@gmail.com>
2021-05-29 13:01:42 -0300

    arrumado o erro do film category service

--- src/main/java/com/residencia/dvdrental/repositories/Film_CategoryRepository.java
+++ src/main/java/com/residencia/dvdrental/repositories/Film_CategoryRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.residencia.dvdrental.entities.Film_Actor;
+import com.residencia.dvdrental.entities.Film_Category;
 
 @Repository
-public interface Film_CategoryRepository extends JpaRepository<Film_Actor, Integer> {
+public interface Film_CategoryRepository extends JpaRepository<Film_Category, Integer> {
 
 }
