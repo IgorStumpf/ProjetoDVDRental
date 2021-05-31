35a1c680bb0a450aaf9d17651d1a23c5ce9eeb30
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-05-31 19:10:55 -0300

    fix: warnings FilmCategoryRepository

--- src/main/java/com/residencia/dvdrental/repositories/Film_CategoryRepository.java
+++ src/main/java/com/residencia/dvdrental/repositories/Film_CategoryRepository.java
@@ -3,7 +3,6 @@ package com.residencia.dvdrental.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.residencia.dvdrental.entities.Film_Actor;
 import com.residencia.dvdrental.entities.Film_Category;
 
 @Repository
